1146f38ce7754ebd5dbcb4008d0048cb887a6645
SkylineCodes <onakoyak@gmail.com>
2023-11-16 11:53:38 +0100

    Completed the common microservice, also completed the reservation crud operations

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
+import { ConfigService, ConfigModule as NestConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
+export class ConfigModule {}
